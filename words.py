9f8eb4181c333793a7be6e4004b869cdf975331a
Pcob <71030608+Pcob@users.noreply.github.com>
2020-11-16 20:09:38 +0100

    Galgje met de woordenlijst
    Deze opdracht heb ik gemaakt met behulp van de volgende YouTube video: https://www.youtube.com/watch?v=m4nEnsavl6w&ab_channel=Kite

---
+++ words.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+word_list = [
+'bedanken',
+'bedenken',
+'bedienen',
+'bedoelen',
+'bedoeling',
+'beurs',
+'beurt',
+'bevallen',
+'bevatten',
+'dankzij',
+'dansen',
+'drijven',
+'dringen',
+'drinken',
+'dromen',
+'erkennen',
+'ernstig',
+'eruitzien',
+'eventjes',
+'eventueel',
+'fout',
+'functie',
+'functioneren']
+
+
+# In[ ]:
+
+
+
+

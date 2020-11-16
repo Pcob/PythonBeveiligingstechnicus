3183a167ea10f05b98d6edfd27a70b7429a1e5de
Pcob <71030608+Pcob@users.noreply.github.com>
2020-11-16 20:06:09 +0100

    Maken van rekenmachine
    Deze rekenmachine heb ik gemaakt met behulp van de volgende YouTube video: https://www.youtube.com/watch?v=62Rbtc5gpsw&ab_channel=MikeDane

---
+++ Rekenmachine.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+print("Hello world")
+
+
+# In[5]:
+
+
+num1 = float(input("Enter first number: "))
+op = input("Enter operator: ")
+num2 = float(input("Enter second number: "))
+
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "/":
+    print(num1 / num2)
+elif op == "*":
+    print(num1 * num2)
+else: 
+    print("Geen geldige operator")
+
+
+# In[6]:
+
+
+print("Hello world")
+
+
+# In[ ]:
+
+
+im
+
